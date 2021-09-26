f41164317d49dedac404b7b5b5b088f4ccc5c083
safayunus <48567645+safayunus@users.noreply.github.com>
2021-09-26 11:53:35 +0300

    Update wait_stats.sql

--- wait_stats.sql
+++ wait_stats.sql
@@ -1,4 +1,4 @@
 SELECT OS.* , S.Session_Id , S.waiting_tasks_count , S.wait_time_ms , S.max_wait_time_ms , S.signal_wait_time_ms 
-FROM sys.dm_os_wait_stats OS INNER JOIN sys.dm_exec_session_wait_stats S ON OS.wait_type = S.wait_type where session_id<> 51
+FROM sys.dm_os_wait_stats OS INNER JOIN sys.dm_exec_session_wait_stats S ON OS.wait_type = S.wait_type where session_id --=51
 
 
