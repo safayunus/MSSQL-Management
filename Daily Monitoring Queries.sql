bd2df913078b8749d33bd0010716999baec9707e
safayunus <48567645+safayunus@users.noreply.github.com>
2022-01-08 08:20:00 +0300

    Update Daily Monitoring Queries.sql

--- Daily Monitoring Queries.sql
+++ Daily Monitoring Queries.sql
@@ -117,3 +117,15 @@ SELECT OBJECT_NAME(object_id)          as table_name
      , STATS_DATE(object_id, index_id) AS StatsUpdated
 FROM sys.indexes
 order by StatsUpdated desc
+
+--How to list users and roles
+select r.name as Role,
+       m.name as Principal
+from master.sys.server_role_members rm
+    inner join master.sys.server_principals r
+        on r.principal_id = rm.role_principal_id
+           and r.type = 'R'
+    inner join master.sys.server_principals m
+        on m.principal_id = rm.member_principal_id
+
+
